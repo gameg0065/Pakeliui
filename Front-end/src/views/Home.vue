320a1cb62be23dc8456860da95d0fd34531acb83
Tomas <kvedaravicius2@gmail.com>
2020-11-02 19:04:09 +0200

    settings

--- Front-end/src/views/Home.vue
+++ Front-end/src/views/Home.vue
@@ -2,17 +2,17 @@
 	<div class="home">
 		<img alt="Vue logo" src="../assets/logo.png" />
 		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+	</div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	components: {
+		HelloWorld
+	}
 });
 </script>
