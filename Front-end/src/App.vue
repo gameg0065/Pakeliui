d96c394349187cd4ca79fa2fb90c214649e56c80
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-05 20:52:21 +0200

    set global css

--- Front-end/src/App.vue
+++ Front-end/src/App.vue
@@ -13,6 +13,9 @@
 </template>
 
 <style lang="scss">
+@import "./assets/styles/_typography.css";
+@import "./assets/styles/_variables.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
