86a78d3d1bfc0380298d32ef1bcdfa0294e388dc
VadimasKo <kozemiakinas123@gmail.com>
2020-11-06 00:03:59 +0200

    DONT Merge, padariau About-us page, noreciau komentaru. 1)width problema yra susijusi su footer komponentu 2)kad pasalinti navbar virsuje- App.vue navbar max-height:0

--- Front-end/src/views/About.vue
+++ Front-end/src/views/About.vue
@@ -1,8 +1,71 @@
 <template>
   <div class="about">
-    <h1>HELLOOOOOOO</h1>
+    <div class="body">
+      <div id="menu">Menu-work in progress</div>
+      <h2>Apie mus</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere est quisquam, illo doloribus tempore numquam blanditiis, velit eligendi accusantium doloremque aspernatur! Nulla veniam molestias pariatur, explicabo doloremque hic velit consequatur.</p>
+      <div class="profiles">
+          <div class="profile">
+              <img src="../assets/images/user-undefined.png" alt="img">
+              <p>Aiste Stonciute</p>
+          </div>
+
+          <div class="profile">
+              <img src="../assets/images/user-undefined.png" alt="img">
+              <p>Ignas Pangonis</p>
+          </div>
+
+          <div class="profile">
+              <img src="../assets/images/user-undefined.png" alt="img">
+              <p>Benjaminas Racas</p>
+          </div>
+
+          <div class="profile">
+              <img src="../assets/images/user-undefined.png" alt="img">
+              <p>Vadimas Kozemiakinas</p>
+          </div>
+
+          <div class="profile">
+              <img src="../assets/images/user-undefined.png" alt="img">
+              <p>Tomas Kvedravicius</p>
+          </div>
+
+          <div class="profile">
+              <img src="../assets/images/user-undefined.png" alt="img">
+              <p>Tomas Sinkunas</p>
+          </div>
+      </div>
+
+      
+      <Footer id="footer">
+        <Footer/>
+      </Footer>
+    </div>
+    
+    
   </div>
+
+
+
+    
+  
 </template>
+
+
+
+
+
+<script lang="ts">
+import Vue from "vue";
+import Footer from "@/components/Footer.vue";
+
+export default Vue.extend({
+  name: "About",
+  components: {
+    Footer
+  }
+});
+</script>
 <style lang="scss">
-  @import "../assets/styles/about.scss"
+@import "../assets/styles/about-us.css";
 </style>
