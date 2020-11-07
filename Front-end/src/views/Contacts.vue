599ff8afaf3c4514897d7d4c36008afb0aebfba3
VadimkasKo <Kozemiakinas123@gmail.com>
2020-11-08 01:11:39 +0200

    "add v-for and data() method for drop down menu + divs for components"

--- Front-end/src/views/Contacts.vue
+++ Front-end/src/views/Contacts.vue
@@ -1,25 +1,53 @@
 <template>
   <div class="body">
     <h2>Kontaktai</h2>
-    <form action="#" class="contact-form" method="post">
-      <label for="full-name">Vardas Pavardė</label>
-      <input  type="text" name="full-name" id="full-name" placeholder="Vardenis Pavardenis" />
-      <label for="email">Elektroninis paštas</label>
-      <inputtype="email" name="email" id="email" placeholder="mano@emailas.lt" />
-      <label for="phone">Telefono numeris</label>
-      <input type="ter" name="phone" id="phone" placeholder="+370-XXX-XXXXX" />
-      <label for="city">Miestas</label>
-      <select name="city" id="city">
-        <option value="Vilnius">Vilnius</option>
-        <option value="Kaunas">Kaunas</option>
-        <option value="Klaipeda">Klaipeda</option>
-        <option value="Taurage">Taurage</option>
-        <option value="Kavenskas">Kavenskas</option>
-      </select>
-      <label for="agenda">Dėstyk savo reikalus čia</label>
-      <input  type="text"  name="agenda" id="agenda" placeholder="State your bussiness"/>
+    <form action="#">
+      <div class="full-name">
+        <label for="full-name">Vardas Pavardė</label>
+        <input type="text" name="full-name" id="full-name" />
+      </div>
+
+      <div class="email">
+        <label for="email">Elektroninis paštas</label>
+        <input type="email" name="email" id="email" />
+      </div>
+      <div class="phone">
+        <label for="phone">Telefono numeris</label>
+        <input type="tel" name="phone" id="phone" />
+      </div>
+
+      <div class="city-drop">
+        <label for="city-drop">Miestas</label>
+        <select name="city-drop" id="city-drop">
+          <option v-for="(city, index) in cities" :key="index" :value="city"
+            >{{ city }}
+          </option>
+        </select>
+      </div>
+
+      <div class="agenda">
+        <label for="agenda">Dėstyk savo reikalus čia</label>
+        <textarea name="agenda" id="agenda" cols="30" rows="3"></textarea>
+      </div>
+
       <button class="submit">Siųsti</button>
     </form>
-    <img src="https://i1.alfi.lt/21081/76/48.png" alt="map Lietuva, Vilnius, Naugarduko g 141"/>
+    <img
+      src="https://i1.alfi.lt/21081/76/48.png"
+      alt="map Lietuva, Vilnius, Naugarduko g 141"
+    />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      cities: ['Vilnius', 'Klaipeda', 'Siauliai', 'Panevezys', 'Alytus']
+    };
+  },
+  created() {
+    this.cities.sort();
+  }
+};
+</script>
