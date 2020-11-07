e8e68eaae06b17c66488a739f25a5c0ec3f5e6ba
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-07 14:29:25 +0200

    cleanup

--- Front-end/src/views/Home.vue
+++ Front-end/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <h1>Geresnis būdas keliauti</h1>
-    <h3>Pakeliui - tai platforma, skirta žmonėms lengvai keliauti bei pavežėti kitus</h3>
+    <h3>
+      Pakeliui - tai platforma, skirta žmonėms lengvai keliauti bei pavežėti
+      kitus
+    </h3>
     <router-link :to="{ name: 'posts' }">
       <Button text="skelbimai" :isLarge="true" />
     </router-link>
@@ -10,17 +13,16 @@
       <Button text="sukurk skelbimą" :isSecondary="true" :isLarge="true" />
     </router-link>
 
-    <img src="../assets/images/hero-image.png" alt="hero">
+    <img src="../assets/images/hero-image.png" alt="hero" />
   </div>
 </template>
 
 <script lang='ts'>
-import Vue from 'vue';
 import Button from '@/components/Button.vue'; // @ is an alias to /src
 
-export default Vue.extend({
+export default {
   components: {
     Button
   }
-});
+};
 </script>
