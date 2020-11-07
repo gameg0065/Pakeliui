bbf5a1afbf52bb4404cdf90360761368b5d30c9f
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-07 22:02:37 +0200

    create Rating.vue component

---
+++ Front-end/src/components/Rating.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img
+      v-for="index in Math.round(rating)"
+      :key="index"
+      src="../assets/icons/star.svg"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    rating: {
+      type: Number,
+      default: 5
+    }
+  }
+};
+</script>
