f7f393caf7d3aa88ac6183dfb83095e6ad974e2e
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-07 14:27:20 +0200

    upload new image and pre-populate home page

--- Front-end/src/views/Home.vue
+++ Front-end/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <h1>Geresnis būdas keliauti</h1>
+    <h3>Pakeliui - tai platforma, skirta žmonėms lengvai keliauti bei pavežėti kitus</h3>
     <router-link :to="{ name: 'posts' }">
       <Button text="skelbimai" :isLarge="true" />
     </router-link>
@@ -7,6 +9,8 @@
     <router-link :to="{ name: 'post-create' }">
       <Button text="sukurk skelbimą" :isSecondary="true" :isLarge="true" />
     </router-link>
+
+    <img src="../assets/images/hero-image.png" alt="hero">
   </div>
 </template>
 
