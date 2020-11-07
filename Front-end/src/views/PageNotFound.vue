a35b8c7c6c1fa2841dd29d7b5883bc24b254caf2
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-08 01:04:56 +0200

    populate component

--- Front-end/src/views/PageNotFound.vue
+++ Front-end/src/views/PageNotFound.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
+  <div class="pageNotFound">
     <h1>404</h1>
-    <p>Is it me you're looking for?</p>
+    <p>Ups, pataikei kur nereikia - toks puslapis neegzistuoja</p>
+    <div>
+      <router-link :to="{ name: 'home' }">
+        <Button
+          class="button"
+          text="grįžti į pagrindinį"
+          :isSecondary="false"
+          :isLarge="true"
+          :isOutlined="true"
+        />
+      </router-link>
+    </div>
+    <img src="../assets/images/404-image.png" />
   </div>
 </template>
+
+<script>
+import Button from '@/components/Button.vue';
+export default {
+  components: {
+    Button
+  }
+};
+</script>
+
+<style>
+.pageNotFound {
+  margin: 0 auto;
+  text-align: center;
+}
+
+.pageNotFound h1 {
+  color: var(--color-secondary);
+  font-size: 100px;
+}
+.pageNotFound .button {
+  margin: 20px;
+}
+</style>
