a4571b16bf94ce093c6ef86819135c6c65b3e2d9
VadimkasKo <Kozemiakinas123@gmail.com>
2020-11-07 17:09:08 +0200

    about-us page(f)

--- Front-end/src/views/About.vue
+++ Front-end/src/views/About.vue
@@ -1,52 +1,52 @@
 <template>
-    <div class="about">
-      <div class="body">
-        <h2>Apie mus</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere est
-          quisquam, illo doloribus tempore numquam blanditiis, velit eligendi
-          accusantium doloremque aspernatur! Nulla veniam molestias pariatur,
-          explicabo doloremque hic velit consequatur.
-        </p>
-        <div class="profiles">
-          <div class="profile">
-            <img src="../assets/images/user-undefined.png" alt="img" />
-            <p>Aiste Stonciute</p>
-          </div>
+  <div class="about">
+    <div class="body">
+      <h2>Apie mus</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere est
+        quisquam, illo doloribus tempore numquam blanditiis, velit eligendi
+        accusantium doloremque aspernatur! Nulla veniam molestias pariatur,
+        explicabo doloremque hic velit consequatur.
+      </p>
+      <div class="profiles">
+        <div class="profile">
+          <img src="../assets/images/user-undefined.png" alt="img" />
+          <p>Aiste Stonciute</p>
+        </div>
 
-          <div class="profile">
-            <img src="../assets/images/user-undefined.png" alt="img" />
-            <p>Ignas Pangonis</p>
-          </div>
+        <div class="profile">
+          <img src="../assets/images/user-undefined.png" alt="img" />
+          <p>Ignas Pangonis</p>
+        </div>
 
-          <div class="profile">
-            <img src="../assets/images/user-undefined.png" alt="img" />
-            <p>Benjaminas Racas</p>
-          </div>
+        <div class="profile">
+          <img src="../assets/images/user-undefined.png" alt="img" />
+          <p>Benjaminas Racas</p>
+        </div>
 
-          <div class="profile">
-            <img src="../assets/images/user-undefined.png" alt="img" />
-            <p>Vadimas Kozemiakinas</p>
-          </div>
+        <div class="profile">
+          <img src="../assets/images/user-undefined.png" alt="img" />
+          <p>Vadimas Kozemiakinas</p>
+        </div>
 
-          <div class="profile">
-            <img src="../assets/images/user-undefined.png" alt="img" />
-            <p>Tomas Kvedravicius</p>
-          </div>
+        <div class="profile">
+          <img src="../assets/images/user-undefined.png" alt="img" />
+          <p>Tomas Kvedravicius</p>
+        </div>
 
-          <div class="profile">
-            <img src="../assets/images/user-undefined.png" alt="img" />
-            <p>Tomas Sinkunas</p>
-          </div>
+        <div class="profile">
+          <img src="../assets/images/user-undefined.png" alt="img" />
+          <p>Tomas Sinkunas</p>
         </div>
       </div>
+    </div>
   </div>
 </template>
 
 
 
 <style lang="scss">
-@import "../assets/styles/about-us.css";
-@import "../assets/styles/_typography.css";
-@import "../assets/styles/_variables.css";
+@import '../assets/styles/about-us.css';
+@import '../assets/styles/_typography.css';
+@import '../assets/styles/_variables.css';
 </style>
