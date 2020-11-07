c71acee4fbb2c6d1a60f062a9cff1faf4e123879
VadimkasKo <Kozemiakinas123@gmail.com>
2020-11-07 18:21:57 +0200

    Salygos-page html

--- Front-end/src/views/Terms.vue
+++ Front-end/src/views/Terms.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <h1>this is SĄLYGOS page</h1>
+  <div class="container">
+    <h2>Vartojimo sąlygos</h2>
+    <p>
+      Galima prideti display flex ir viskas job done xD Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Molestiae blanditiis, exercitationem
+      necessitatibus, dolorum sapiente iusto et fuga soluta at corrupti
+      molestias voluptate nemo possimus quia saepe voluptatibus ratione illo
+      facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+      repudiandae unde dolorem in maiores ipsum enim praesentium. Ad et sapiente
+      beatae tenetur odit praesentium quas voluptatum distinctio. Recusandae,
+      ullam corrupti!
+    </p>
+    <p>
+      i know its boring so dont read it amet consectetur adipisicing elit.
+      Molestiae blanditiis, exercitationem necessitatibus, dolorum sapiente
+      iusto et fuga soluta at corrupti molestias voluptate nemo possimus quia
+      saepe voluptatibus ratione illo facere.
+    </p>
   </div>
 </template>
