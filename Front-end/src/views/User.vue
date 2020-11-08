d00b6d07e8f62c67eda5e30acf535610e551d115
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-08 19:42:20 +0200

    add avatar

--- Front-end/src/views/User.vue
+++ Front-end/src/views/User.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
     <h1>this is USER # {{ id }} page</h1>
+    <h1>this is USER # {{ typeof id }} page</h1>
+    <Avatar :path="user.photo" size="big" />
+    <p>{{ user.name }}</p>
   </div>
 </template>
 
 <script>
+import Avatar from '@/components/Avatar.vue';
+import UserService from '@/services/UserService.js';
+
 export default {
   props: ['id'],
+  components: {
+    Avatar
+  },
+  data() {
+    return {
+      user: {}
+    };
+  },
   created() {
-    // fetch user data from DB here
+    const id = parseInt(this.id);
+    this.user = UserService.getUser(id);
   }
 };
 </script>
