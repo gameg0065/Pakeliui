1d0cabb2a1b50a5dfc6539629b9b25e9964d3db9
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-08 16:45:35 +0200

    set up vuex with dummy data

--- Front-end/src/store/index.ts
+++ Front-end/src/store/index.ts
@@ -4,8 +4,25 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+	state: {
+		user: {
+			id: 100,
+			isLoggedIn: false
+		}
+	},
+	getters: {
+		isLoggedIn: state => {
+			return state.user.isLoggedIn;
+		},
+		userID: state => {
+			return state.user.id;
+		}
+	},
+	mutations: {
+		SET_LOGGED_IN(state, value) {
+			state.user.isLoggedIn = value;
+		}
+	},
+	actions: {},
+	modules: {}
 });
