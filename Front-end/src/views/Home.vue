24c898f61533b3364f74d786ea90b7ab5de8e6f9
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-08 21:17:34 +0200

    links home buttons to login/register modals

--- Front-end/src/views/Home.vue
+++ Front-end/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <ModalLogin />
+    <ModalRegister />
+
     <h1>Geresnis būdas keliauti</h1>
     <h3>
       Pakeliui - tai platforma, skirta žmonėms lengvai keliauti bei pavežėti
@@ -15,8 +18,19 @@
       </router-link>
     </div>
     <div v-if="!isLoggedIn">
-      <Button text="prisijungti" :isSecondary="false" :isLarge="true" />
-      <Button text="užsiregistruoti" :isSecondary="true" :isLarge="true" />
+      <Button
+        :click="openModalLogin"
+        text="prisijungti"
+        :isSecondary="false"
+        :isLarge="true"
+      />
+
+      <Button
+        :click="openModalRegister"
+        text="užsiregistruoti"
+        :isSecondary="true"
+        :isLarge="true"
+      />
     </div>
 
     <img src="../assets/images/hero-image.png" alt="hero" />
@@ -25,14 +39,27 @@
 
 <script lang='ts'>
 import Button from '@/components/Button.vue';
+import ModalLogin from '@/components/ModalLogin.vue';
+import ModalRegister from '@/components/ModalRegister.vue';
 
 import { mapGetters } from 'vuex';
 export default {
   components: {
-    Button
+    Button,
+    ModalLogin,
+    ModalRegister
   },
   computed: {
     ...mapGetters(['isLoggedIn'])
+  },
+  methods: {
+    openModalLogin() {
+      this.$modal.show('modal-login');
+    },
+    openModalRegister() {
+      this.$modal.show('modal-register');
+    }
   }
 };
 </script>
+
