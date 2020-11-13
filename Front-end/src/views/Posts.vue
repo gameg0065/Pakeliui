56a01a36c0d6c55b0c75329fa04b9db1565c497c
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-13 23:12:13 +0200

    adds PostFiler component into Posts.vue component

--- Front-end/src/views/Posts.vue
+++ Front-end/src/views/Posts.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
     <h2>Skelbimai</h2>
+    <PostFilter />
     <PostCard v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
 
 <script>
 import PostCard from '@/components/PostCard.vue';
+import PostFilter from '@/components/PostFilter.vue';
+
 import PostService from '@/services/PostService.js';
 
 export default {
   components: {
-    PostCard
+    PostCard,
+    PostFilter,
   },
   data() {
     return {
-      posts: []
+      posts: [],
     };
   },
   created() {
     this.posts = PostService.getPosts();
-  }
+  },
 };
 </script>
