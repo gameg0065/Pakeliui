48a30e4acfc5bbdf6b8f2bafa6b3f092f61185a5
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-13 18:44:29 +0200

    create DriverActivePostCard.vue component

---
+++ Front-end/src/components/DriverActivePostCard.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="card shadow">
+    <div>
+      <small>Kelionės data</small>
+      <p>{{ post.date }}</p>
+    </div>
+
+    <div>
+      <small>Kelionės laikas</small>
+      <p>{{ post.time }}</p>
+    </div>
+
+    <div>
+      <small>Iš miesto</small>
+      <p>{{ post.route.from }}</p>
+    </div>
+
+    <div>
+      <small>Paėmimo vieta</small>
+      <p>{{ post.route.pickup }}</p>
+    </div>
+
+    <div>
+      <small>Į miestą</small>
+      <p>{{ post.route.to }}</p>
+    </div>
+
+    <div>
+      <small>Pristatymo vieta</small>
+      <p>{{ post.route.dropoff }}</p>
+    </div>
+
+    <div>
+      <small>Kiek gali paimti keleivių</small>
+      <p>{{ post.seetCount }}</p>
+    </div>
+
+    <div>
+      <small>Patvirtinti keleiviai</small>
+      <Avatar
+        v-for="user in getApprovedPassengers(post, 'TAKEN')"
+        :key="user.id"
+        :path="user.photo"
+      />
+    </div>
+
+    <div>
+      <small>Keleiviai, laukiantys patvirtinimo</small>
+      <Avatar
+        v-for="user in getApprovedPassengers(post, 'PENDING')"
+        :key="user.id"
+        :path="user.photo"
+      />
+    </div>
+
+    <Button text="peržiūrėti skelbimą" :isOutlined="true" />
+    <Button text="ištrinti" :isOutlined="true" :isSecondary="true" />
+    <Button text="redaguoti" :isOutlined="true" />
+  </div>
+</template>
+
+<script>
+import Avatar from '@/components/Avatar.vue';
+import Button from '@/components/Button.vue';
+
+import UserService from '@/services/UserService.js';
+
+export default {
+  props: ['post'],
+  components: {
+    Avatar,
+    Button,
+  },
+  methods: {
+    getApprovedPassengers(post, status) {
+      const users = [];
+
+      post.passengers.forEach(function (passenger) {
+        if (passenger.status === status) {
+          const user = UserService.getUser(passenger.id);
+          users.push(user);
+        }
+      });
+
+      return users;
+    },
+  },
+};
+</script>
