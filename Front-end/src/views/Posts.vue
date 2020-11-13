e383b63b7bb1f3fd0cef63dc343cac866c7f5bcb
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-14 00:16:30 +0200

    Posts.vue component now filters posts

--- Front-end/src/views/Posts.vue
+++ Front-end/src/views/Posts.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>Skelbimai</h2>
-    <PostFilter />
+    <PostFilter @post-filter-changed="onPostFilterChanged" />
     <PostCard v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
@@ -24,6 +24,30 @@ export default {
   },
   created() {
     this.posts = PostService.getPosts();
+  },
+  methods: {
+    onPostFilterChanged(route, date) {
+      const posts = PostService.getPosts();
+      this.posts = posts.filter(function (post) {
+        if (route.from && post.route.from !== route.from) {
+          return false;
+        }
+        
+        if (route.to && post.route.to !== route.to) {
+          return false;
+        }
+
+        if (date) {
+          const postDate = new Date(post.date);
+          const routeDate = new Date(date);
+
+          if (postDate.getTime() <= routeDate.getTime()) {
+            return false;
+          }
+        }
+        return true;
+      });
+    },
   },
 };
 </script>
