fbcdd54492562aecb537882f86f5ad15e616c705
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-14 22:39:13 +0200

    hooks "Rezervuoti" buttons in Post.vue component

--- Front-end/src/views/Post.vue
+++ Front-end/src/views/Post.vue
@@ -8,7 +8,12 @@
 
     <div>
       <UserCardInPost :post="post" />
-      <Button v-if="isActive" text="rezervuoti" :isOutlined="true" />
+      <Button
+        v-if="isActive"
+        text="rezervuoti"
+        :click="reserve"
+        :isOutlined="true"
+      />
     </div>
 
     <div>
@@ -56,7 +61,7 @@
 
     <img src="https://i.stack.imgur.com/yEshb.gif" alt="map" />
 
-    <Button v-if="isActive" text="REZERVUOTI" />
+    <Button v-if="isActive" text="rezervuoti" :click="reserve" />
     <Comments :comments="post.comments" :isActive="isActive" />
   </div>
 </template>
@@ -88,6 +93,11 @@ export default {
     const postDate = new Date(this.post.date);
 
     this.isActive = postDate.getTime() >= now.getTime();
+  },
+  methods: {
+    reserve() {
+      alert('TODO');
+    },
   },
 };
 </script>
