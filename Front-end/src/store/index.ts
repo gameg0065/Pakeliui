b59e7cf694773cbdbfe7b2108db298fc3535cbda
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-15 23:34:01 +0200

    finish setting up vuex

--- Front-end/src/store/index.ts
+++ Front-end/src/store/index.ts
@@ -5,56 +5,19 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
-		user: {
-			id: 110,
-			isDriver: false,
-			isLoggedIn: false
-		},
-		user2: null
+		user: null
 	},
 	getters: {
-		loggedIn(state) {
-			return !!state.user2;
-		},
-		isDriver: state => {
-			return state.user.isDriver;
-		},
-		isLoggedIn: state => {
-			return state.user.isLoggedIn;
-		},
-		userID: state => {
-			return state.user.id;
-		},
-
-		/////
-
 		getUser: state => {
-			return state.user2;
+			return state.user;
 		}
 	},
 	mutations: {
-		SET_IS_DRIVER(state, value) {
-			state.user.isDriver = value;
-		},
-		SET_LOGGED_IN(state, value) {
-			state.user.isLoggedIn = value;
-			if (!value) {
-				state.user.id = 0;
-				state.user.isDriver = false;
-				state.user.isLoggedIn = false;
-			}
-		},
-		SET_USER_ID(state, id) {
-			state.user.id = id;
-		},
-
-		/////
-
 		CLEAR_USER_DATA(state) {
-			state.user2 = null;
+			state.user = null;
 		},
 		SET_USER(state, user) {
-			state.user2 = user;
+			state.user = user;
 		}
 	},
 	actions: {
