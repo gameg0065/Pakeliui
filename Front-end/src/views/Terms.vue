8e4e8de6a075c4892375977c261d219b1f2b95c0
VadimkasKo <Kozemiakinas123@gmail.com>
2020-11-16 18:04:11 +0200

    Update salygos text

--- Front-end/src/views/Terms.vue
+++ Front-end/src/views/Terms.vue
@@ -1,21 +1,181 @@
 <template>
-  <div class="container">
+  <div class="terms">
     <h2>Vartojimo sąlygos</h2>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-      blanditiis, exercitationem necessitatibus, dolorum sapiente iusto et fuga
-      soluta at corrupti molestias voluptate nemo possimus quia saepe
-      voluptatibus ratione illo facere. Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Sapiente repudiandae unde dolorem in maiores ipsum enim
-      praesentium. Ad et sapiente beatae tenetur odit praesentium quas
-      voluptatum distinctio. Recusandae, ullam corrupti!
+      Naudodamiesi „Pakeliui“ teikiamomis paslaugomis, Jūs sutinkate būti
+      saistomas šių sąlygų („Sąlygos“). Prieš naudojantis „Pakeliui“
+      paslaugomis, prašome atidžiai perskaityti toliau išdėstytas sąlygas. Jei
+      jūs nesutinkate su sąlygomis, prašome nesinaudoti „Pakeliui“.
     </p>
-    <p>
-      ... amet consectetur adipisicing elit. Molestiae blanditiis,
-      exercitationem necessitatibus, dolorum sapiente iusto et fuga soluta at
-      corrupti molestias voluptate nemo possimus quia saepe voluptatibus ratione
-      illo facere.
-    </p>
+    <ol>
+      <li>APIBRĖŽIMAI</li>
+      <ol>
+        <li>
+          „Pakeliui“ - tai tinklapis: www.pakeliui.eu, toliau vadinami
+          „Tinklapiu“ arba „Platforma“.
+        </li>
+        <li>
+          „Pakeliui komanda“ (taip pat vadinami „mes“, „mūsų“, arba „mus“) –
+          Platformos „Pakeliui“ įkūrėjai.
+        </li>
+        <li>
+          „Vartotojas“ – fizinis asmuo, kuris naudojasi „Pakeliui“ teikiamomis
+          paslaugomis.
+        </li>
+        <li>
+          „Vairuotojas“ – Vartotojas, kuris planuoja konkretų maršrutą tam tikru
+          laiku ir siūlo keleiviui pavėžėjimo paslaugą savo transporto
+          priemonėje.
+        </li>
+        <li>
+          Keleivis“ – Vartotojas, kuris planuoja pasinaudoti ar pasinaudojo
+          Vairuotojo pavėžėjimo paslauga konkrečiu maršrutu tam tikru laiku.
+        </li>
+        <li>
+          „Vartotojo paskyra“ arba „Paskyra“ – Vartotojo registracija
+          Platformoje norint naudotis Platforma; sukurtoje paskyroje nurodomi
+          asmens duomenys.
+        </li>
+        <li>
+          „Asmeninės žinutės“ – Vartotojų bendravimas tarpusavyje privačiomis
+          žinutėmis per Tinklapio žinučių sistemą.
+        </li>
+        <li>
+          „Kelionė“ – konkretus maršrutas konkrečiu laiku, Vairuotojo paskelbtas
+          Platformoje.
+        </li>
+        <li>
+          „Skelbimas“ – Vairuotojo paskelbtas įrašas, kuriame nurodoma Kelionė.
+        </li>
+        <li>
+          „Vartotojų sutartis“ – sutartis tarp Vairuotojo ir Keleivio dėl
+          teikiamos paslaugos – Kelionės.
+        </li>
+        <li>
+          „Paslauga“ – tai Platformoje Vairuotojo teikiamos pavėžėjimo
+          paslaugos.
+        </li>
+      </ol>
+
+      <li>BENDROSIOS PASLAUGŲ TEIKIMO SĄLYGOS</li>
+      <ol>
+        <li>
+          Pakeliui yra internetinė tarpininkavimo tarp Keleivio ir Vairuotojo
+          paslaugas teikianti bendrovė, kur Vartotojai gali parašyti Skelbimą
+          arba rezervuoti Kelionę.
+        </li>
+        <li>Šios Sąlygos taikomos visiems Vartotojams.</li>
+        <li>Sukūręs Paskyrą Vartotojas sutinka laikytis šių Sąlygų.</li>
+        <li>
+          Sukūręs Paskyrą Vartotojas sutinka, kad jo asmens duomenys būtų
+          tvarkomi vykdant Konfidencialumo politikos reikalavimus.
+        </li>
+        <li>
+          Pagrindinė informacija apie Kelionę – Kelionės pradžia, Kelionės
+          pabaiga, Kelionės pradžios laikas, Kelionės data privalo būti
+          suderinta Platformos sistemoje.
+        </li>
+      </ol>
+
+      <li>INFORMACIJOS TALPINIMAS PLATFORMOJE IR NAUDOJIMASIS PLATFORMA</li>
+      <ol>
+        <li>
+          Paslaugos neskirtos naudoti jaunesniems nei 18 metų amžiaus asmenims.
+          Jei Jums mažiau nei 18 metų, registruotis ir Jūsų vardu naudotis
+          Paslaugomis privalo vienas iš Jūsų tėvų ar globėjų (kuriam yra daugiau
+          nei 18 metų). Jūsų tėvai ar globėjai visada yra atsakingi už Jūsų
+          priežiūra Jums naudojantis Paslaugomis. Jei pažeisite bet kurią iš šių
+          Sąlygų nuostatų, Pakeliui turi teisė laikyti Jūsų tėvus ar globėjus
+          atsakingais.
+        </li>
+        <li>
+          Asmuo gali naudotis Paslaugomis tik susikūręs Paskyrą ir tapęs
+          Vartotoju. Paskyroje privalo būti nurodyti asmens duomenys: vardas,
+          pavardė, elektroninis paštas, telefono numeris.
+        </li>
+        <li>
+          Vienas Vartotojas turi teisę sukurti tik vieną paskyrą. Naudotis
+          svetimomis Paskyromis arba daugiau nei viena Paskyra yra griežtai
+          draudžiama
+        </li>
+        <li>
+          Platformoje talpindamas bet kokią informaciją Vartotojas sutinka, kad
+          Platformos komanda turi teisę tvarkyti šią informaciją bei bet kokiu
+          būdu ją naudoti ir saugoti.
+        </li>
+        <li>
+          Vartotojas įsipareigoja Platformoje netalpinti melagingos,
+          įžeidžiančios, klaidinančios, kitos draudžiamos informacijos,
+          pažeidžiančios kitų asmenų teises. Taip pat Vartotojas įsipareigoja
+          Platformoje netalpinti reklaminės medžiagos.
+        </li>
+        <li>
+          Pakeliui komandai pareikalavus, Vartotojas įsipareigoja pašalinti
+          informaciją iš sistemos, jei yra pagrindo manyti, kad paskelbta
+          informacija neatitinka teisės aktų reikalavimų.
+        </li>
+      </ol>
+
+      <li>VAIRUOTOJO PAREIGOS</li>
+      <ol>
+        <li>
+          Už bet kokią žalą, padarytą Keleiviui ar tretiesiems asmenims,
+          Vairuotojas atsako pagal galiojančius įstatymus.
+        </li>
+        <li>Griežtai laikytis Kelių eismo taisyklių</li>
+        <li>
+          Pasikeitus Kelionės sąlygoms asmeniškai susisiekti su kiekvienu
+          Keleiviu, sudariusiu su juo Vartotojų sutartį, ir suderinti naujas
+          Kelionės sąlygas.
+        </li>
+        <li>
+          Turėti galiojantį vairuotojo pažymėjimą, galiojančius transporto
+          priemonės registravimo ir techninės apžiūros dokumentus bei kitus
+          būtinus dokumentus. Keleiviui pareikalavus, Vairuotojas privalo
+          pateikti šiuos dokumentus susipažinimui.
+        </li>
+        <li>
+          Užtikrinti švarą ir tvarką konkrečiai kelionei skirtai transporto
+          priemonei.
+        </li>
+        <li>Laikytis etikos normų.</li>
+        <li>
+          Asmeniškai atvykti į Vartotojų sutartyje sutartą vietą su sutarta
+          transporto priemone ir laukti sutartą laiko tarpą.
+        </li>
+      </ol>
+
+      <li>VARTOTOJŲ TEISĖS</li>
+      <ol>
+        <li>
+          Vartotojas sutinka, kad Pakeliui komanda turi teisę bet kuriuo momentu
+          savo nuožiūra apriboti Vartotojui galimybę naudotis sistema. Pakeliui
+          komanda taip pat turi teisę pašalinti Paskyras arba jas sustabdyti.
+        </li>
+      </ol>
+
+      <li>SĄLYGŲ PAKEITIMAI</li>
+      <ol>
+        <li>
+          Sąlygų pakeitimai įsigalioja nuo to momento, kai jos yra paskelbiamos
+          Platformoje.
+        </li>
+      </ol>
+
+      <li>BAIGIAMOSIOS NUOSTATOS</li>
+      <ol>
+        <li>
+          Taisyklės įsigalios nuo to momento, kai Vartotojas patvirtins, kad
+          sutinka su jomis ir galios visą santykių tarp Vartotojo ir Pakeliui
+          laiką.
+        </li>
+        <li>
+          Vartotojas prašymą ar skundą dėl Pakeliui veiklos ir teikiamų paslaugų
+          pirmiausia privalo pateikti Platformai Pakeliui ir nurodyti savo
+          reikalavimus.
+        </li>
+      </ol>
+    </ol>
   </div>
 </template>
 
