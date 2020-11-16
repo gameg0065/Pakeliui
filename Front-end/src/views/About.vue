754d1a7d26d139981719f0e307afac2fd1e4f275
VadimkasKo <Kozemiakinas123@gmail.com>
2020-11-16 19:17:18 +0200

    text to default, add missing dots

--- Front-end/src/views/About.vue
+++ Front-end/src/views/About.vue
@@ -3,9 +3,15 @@
     <div class="body">
       <h2>Apie mus</h2>
       <h4>Kas yra „Pakeliui“?</h4>
+      <p>Tai lietuviška platforma, skirta visiems, norintiems keliauti kartu su
+        pakeleiviais.
+      </p>
       <p>
-        Tai lietuviška platforma, skirta visiems, norintiems keliauti kartu su
-        pakeleiviais. Projekto idėja gimė iš studentiško gyvenimo patirties.
+        Pakeliui“ įkūrėjai – tai jauna komanda, kurią sudaro ambicingi, veržlūs ir nuolat tobulėjantys studentai. Tai yra pelno siekianti organizacija, kurios pagrindinis tikslas – teikti kokybišką pavėžėjimo tarpininkavimo paslaugą. Mūsų komandos vertybes sudaro profesionalumas, kokybė, atsakomybė ir inovatyvumas. 
+      </p>
+
+      <p>
+        Projekto idėja gimė iš studentiško gyvenimo patirties.
         Važiavimas tarpmiestiniu transportu gali prailgti, tapti varginančiu bei
         gerokai ištuštinti piniginę. Kelionės kartu su nepažįstamais
         vairuotojais atrodė patogesnė, tačiau mažiau saugi alternatyva. Būtent
@@ -18,17 +24,14 @@
       <h4>Mūsų tikslas – užtikrinti keliavimo būdą, kuris būtų:</h4>
       <p>
         <ul>
-          <li><strong>Saugus</strong> - visa informacija apie vartotojus bei keliones yra saugoma</li>
+          <li><strong>Saugus</strong> - visa informacija apie vartotojus bei keliones yra saugoma.</li>
           <li><strong>Greitas</strong> - skelbimus norimu laiku rasti padeda platformoje esantys filtrai. </li>
           <li><strong>Pigus</strong> - sutaupyti padeda kelionės kaštų dalijimasis su pakeleiviais.</li>
           <li><strong>Patogus </strong> - platforma leidžia rasti bendrakeleivių visoje Lietuvoje. </li>
-          <li><strong>Pilnas galimybių</strong> - pakeliui gali sutikti begalės unikalių žmonių</li>
+          <li><strong>Pilnas galimybių</strong> - pakeliui gali sutikti begalės unikalių žmonių.</li>
         </ul>
       </p>
       <h4>„Pakeliui“ komanda </h4>
-      <p>
-        „Pakeliui“ įkūrėjai – tai jauna komanda, kurią sudaro ambicingi, veržlūs ir nuolat tobulėjantys studentai. Tai yra pelno siekianti organizacija, kurios pagrindinis tikslas – teikti kokybišką pavėžėjimo tarpininkavimo paslaugą. Mūsų komandos vertybes sudaro profesionalumas, kokybė, atsakomybė ir inovatyvumas. 
-      </p>
 
       <div class="profiles">
         <div class="profile">
