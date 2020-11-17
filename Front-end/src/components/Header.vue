a30381ede8df5cd622149308fe64d73616ad175b
VadimkasKo <Kozemiakinas123@gmail.com>
2020-11-17 21:47:53 +0200

    add class container for header- to avoid conflicts, update about-us.css

--- Front-end/src/components/Header.vue
+++ Front-end/src/components/Header.vue
@@ -1,39 +1,41 @@
 <template>
-  <nav>
-    <ul class="left-links">
-      <li>
-        <router-link :to="{ name: 'home' }"
-          ><img src="../assets/images/logo-pakeliui.svg" class="logo"
-        /></router-link>
-      </li>
-      <li v-if="user">
-        <router-link :to="{ name: 'posts' }">
-          <Button text="skelbimai" :isSecondary="true" />
-        </router-link>
-      </li>
-      <li v-if="user">
-        <router-link v-if="user.isDriver" :to="{ name: 'post-create' }">
-          <Button text="sukurk skelbimą" :isSecondary="true" />
-        </router-link>
-      </li>
-    </ul>
-    <ul class="right-links">
-      <li v-if="!user">
-        <a href="#" @click.prevent="$modal.show('modal-login')">prisijungti</a>
-      </li>
-      <li v-if="!user">
-        <a href="#" @click.prevent="$modal.show('modal-register')"
-          >registruotis</a
-        >
-      </li>
-      <li v-if="user">
-        <a href="#" @click.prevent="$emit('on-profile-button-click')">
-          profilis
-          <Avatar :path="user.photo" size="small" />
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <div class="header">
+    <nav >
+      <ul class="left-links">
+        <li>
+          <router-link :to="{ name: 'home' }"
+            ><img src="../assets/images/logo-pakeliui.svg" class="logo"
+          /></router-link>
+        </li>
+        <li v-if="user">
+          <router-link :to="{ name: 'posts' }">
+            <Button text="skelbimai" :isSecondary="true" />
+          </router-link>
+        </li>
+        <li v-if="user">
+          <router-link v-if="user.isDriver" :to="{ name: 'post-create' }">
+            <Button text="sukurk skelbimą" :isSecondary="true" />
+          </router-link>
+        </li>
+      </ul>
+      <ul class="right-links">
+        <li v-if="!user">
+          <a href="#" @click.prevent="$modal.show('modal-login')">prisijungti</a>
+        </li>
+        <li v-if="!user">
+          <a href="#" @click.prevent="$modal.show('modal-register')"
+            >registruotis</a
+          >
+        </li>
+        <li v-if="user">
+          <a href="#" @click.prevent="$emit('on-profile-button-click')">
+            profilis
+            <Avatar :path="user.photo" size="small" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss">
