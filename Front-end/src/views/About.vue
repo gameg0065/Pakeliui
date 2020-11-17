5bf1f5f32a0f894ad5a894ad88037155adada58e
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-17 21:23:56 +0200

    update Apie Mus texts

--- Front-end/src/views/About.vue
+++ Front-end/src/views/About.vue
@@ -2,23 +2,21 @@
   <div class="about">
     <div class="body">
       <h2>Apie mus</h2>
-      <p>Pakeliui – lengvesnis būdas keliauti. </p>
+      <p>Pakeliui – geresnis būdas keliauti!</p>
       <h4>Kas yra „Pakeliui“?</h4>
-      <p>Tai lietuviška platforma, skirta visiems, norintiems keliauti kartu su pakeleiviais.</p>
-      <p>„Pakeliui“ įkūrėjai – tai jauna komanda, kurią sudaro ambicingi, veržlūs ir nuolat tobulėjantys studentai. Tai yra pelno siekianti organizacija, kurios pagrindinis tikslas – teikti kokybišką pavėžėjimo tarpininkavimo paslaugą. Mūsų komandos vertybes sudaro profesionalumas, kokybė, atsakomybė ir inovatyvumas.  </p>
-
-      <p>Projekto idėja gimė iš studentiško gyvenimo patirties. Važiavimas tarpmiestiniu transportu gali prailgti, tapti varginančiu bei gerokai ištuštinti piniginę. Kelionės kartu su nepažįstamais vairuotojais atrodė patogesnė, tačiau mažiau saugi alternatyva. Būtent todėl užsibrėžėme tikslą sukurti internetinę platformą, kuri šį keliavimo būdą paverstu jaukiu ir saugiu. </p>
-      <p>„Pakeliui" Jums siūlo pigesnį, lengvesnį bei patogesnį keliavimo būdą. Važiavimas kartu su pakeleiviais skirtas ne tik studentams, bet visiems, norintiems iš taško A nukeliauti į tašką B greitai, saugiai ir pigiai. Mūsų komandos sukurta platforma – tarpininkas tarp vairuotojo ir keleivio, užtikrinantis sklandų bendradarbiavimą. </p>
-      <h4>Mūsų tikslas – užtikrinti keliavimo būdą, kuris būtų:</h4>
+      <p>Tai lietuviška platforma, skirta visiems, norintiems keliauti kartu su pakeleiviais</p>
+      <p>„Pakeliui“ įkūrėjai – tai jauna komanda, kurią sudaro ambicingi, veržlūs ir nuolat tobulėjantys studentai. Tai yra pelno siekianti organizacija, kurios pagrindinis tikslas – teikti kokybišką pavėžėjimo tarpininkavimo paslaugą. Mūsų komandos vertybes sudaro profesionalumas, kokybė, atsakomybė ir inovatyvumas.</p>
+      <p>Projekto idėja gimė iš studentiško gyvenimo patirties. Važiavimas tarpmiestiniu transportu gali prailgti, tapti varginančiu bei gerokai ištuštinti piniginę. Kelionės kartu su nepažįstamais vairuotojais atrodo patogesnė, tačiau mažiau saugi alternatyva. Būtent todėl užsibrėžėme tikslą sukurti internetinę platformą, kuri šį keliavimo būdą paverstų jaukiu ir visiškai saugiu.</p>
+      <p>„Pakeliui" Jums siūlo pigesnį, lengvesnį bei patogesnį keliavimo būdą. Važiavimas kartu su pakeleiviais skirtas ne tik studentams, bet visiems, norintiems iš taško A nukeliauti į tašką B greitai, saugiai ir pigiai. Mūsų komandos sukurta platforma – tarpininkas tarp vairuotojo ir keleivio, užtikrinantis sklandų bendradarbiavimą. Mūsų tikslas – užtikrinti keliavimo būdą, kuris būtų:</p>
       <ul>
         <li><strong>Saugus</strong> - visa informacija apie vartotojus bei keliones yra saugoma.</li>
-        <li><strong>Greitas</strong> - skelbimus norimu laiku rasti padeda platformoje esantys filtrai. </li>
+        <li><strong>Greitas</strong> - skelbimus norimu laiku rasti padeda platformoje esantys filtrai.</li>
         <li><strong>Pigus</strong> - sutaupyti padeda kelionės kaštų dalijimasis su pakeleiviais.</li>
-        <li><strong>Patogus </strong> - platforma leidžia rasti bendrakeleivių visoje Lietuvoje. </li>
-        <li><strong>Pilnas galimybių</strong> - pakeliui gali sutikti begalės unikalių žmonių.</li>
+        <li><strong>Patogus </strong> - platforma leidžia rasti bendrakeleivių visoje Lietuvoje.</li>
+        <li><strong>Pilnas galimybių</strong> - pakeliui gali sutikti begales unikalių žmonių</li>
       </ul>
-    
-      <h4>„Pakeliui“ komanda </h4>
+
+      <h4>„Pakeliui“ komanda:</h4>
 
       <div class="profiles">
         <div class="profile">
