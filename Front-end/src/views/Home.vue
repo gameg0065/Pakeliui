f08851c31953282b0cd2a0cbeec5f20b93c5a02f
VadimkasKo <Kozemiakinas123@gmail.com>
2020-11-17 19:04:57 +0200

    Home css ,  move img to background

--- Front-end/src/views/Home.vue
+++ Front-end/src/views/Home.vue
@@ -1,38 +1,49 @@
 <template>
   <div class="home">
-    <h1>Geresnis būdas keliauti</h1>
-    <h3>
-      Pakeliui - tai platforma, skirta žmonėms lengvai keliauti bei pavežėti
-      kitus
-    </h3>
-    <div v-if="user">
-      <router-link :to="{ name: 'posts' }">
-        <Button text="skelbimai" :isLarge="true" />
-      </router-link>
+    <div class="home-box">
+      <h1>Geresnis būdas keliauti</h1>
+      <h3>
+        Pakeliui - tai platforma, skirta žmonėms lengvai keliauti bei pavežėti
+        kitus
+      </h3>
+      <div v-if="user">
+        <router-link :to="{ name: 'posts' }">
+          <Button class="home-buttons" text="skelbimai" :isLarge="true" />
+        </router-link>
 
-      <router-link v-if="user.isDriver" :to="{ name: 'post-create' }">
-        <Button text="sukurk skelbimą" :isSecondary="true" :isLarge="true" />
-      </router-link>
+        <router-link v-if="user.isDriver" :to="{ name: 'post-create' }">
+          <Button
+            class="home-buttons"
+            text="sukurk skelbimą"
+            :isSecondary="true"
+            :isLarge="true"
+          />
+        </router-link>
+      </div>
+      <div v-if="!user">
+        <Button
+          class="home-buttons"
+          :click="openModalLogin"
+          text="prisijungti"
+          :isSecondary="false"
+          :isLarge="true"
+        />
+
+        <Button
+          class="home-buttons"
+          :click="openModalRegister"
+          text="užsiregistruoti"
+          :isSecondary="true"
+          :isLarge="true"
+        />
+      </div>
     </div>
-    <div v-if="!user">
-      <Button
-        :click="openModalLogin"
-        text="prisijungti"
-        :isSecondary="false"
-        :isLarge="true"
-      />
-
-      <Button
-        :click="openModalRegister"
-        text="užsiregistruoti"
-        :isSecondary="true"
-        :isLarge="true"
-      />
-    </div>
-
-    <img src="../assets/images/hero-image.png" alt="hero" />
   </div>
 </template>
+
+<style lang="scss">
+@import '../assets/styles/home.css';
+</style>
 
 <script>
 import Button from '@/components/Button.vue';
