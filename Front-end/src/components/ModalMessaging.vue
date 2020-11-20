ab966812eeb29468d1005e25e08f43f31a3779a4
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-20 23:43:50 +0200

    create ModalMessaging.vue component

---
+++ Front-end/src/components/ModalMessaging.vue
@@ -0,0 +1,63 @@
+<template>
+  <modal
+    name="modal-messaging"
+    :adaptive="true"
+    :height="400"
+    :width="600"
+    :styles="{ 'border-radius': '10px' }"
+    :focusTrap="true"
+    @before-open="beforeOpen"
+  >
+    <div class="container">
+      <h3>{{ params.title }}</h3>
+      <p>{{ params.text }}</p>
+
+      <div>
+        <textarea cols="30" rows="3"></textarea>
+      </div>
+
+      <Button :text="button.title" :click="button.action" :isOutlined="true" />
+    </div>
+  </modal>
+</template>
+
+<script>
+import Button from '@/components/Button.vue';
+
+export default {
+  name: 'ModalMessaging',
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      params: {},
+    };
+  },
+  computed: {
+    button() {
+      return this.params.button || {};
+    },
+  },
+  methods: {
+    hide() {
+      this.$modal.hide('modal-messaging');
+    },
+    show() {
+      this.$modal.show('modal-messaging');
+    },
+    beforeOpen(event) {
+      this.params = event.params || {};
+    },
+  },
+  mount() {
+    this.show();
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  padding: 20px;
+}
+</style>
