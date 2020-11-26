8c5b599fee4b50bacb8869af3818125a5b29b610
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-26 17:57:44 +0200

    do User-Edit.vue css

--- Front-end/src/views/UserEdit.vue
+++ Front-end/src/views/UserEdit.vue
@@ -1,159 +1,174 @@
 <template>
-  <div>
-    <div>
-      <h2>Profilio redagavimas</h2>
-
-      <div>
+  <div class="user-edit">
+    <h2 class="page-title">Profilio redagavimas</h2>
+    <div class="flex pb-50">
+      <div class="flex direction-column align-center">
         <Avatar :path="user.photo" size="big" />
         <Button
           text="įkelti nuotrauką"
           :click="changeUserPhoto"
           :isOutlined="true"
+          class="mt-20"
         />
       </div>
 
-      <div>
-        <label for="user-name">Vardas, pavardė*</label>
-        <input type="text" id="user-name" v-model.trim="user.name" />
-      </div>
+      <div class="flex direction-column grow">
+        <div class="flex align-baseline">
+          <label for="user-name">Vardas, pavardė*</label>
+          <input type="text" id="user-name" v-model.trim="user.name" />
+        </div>
 
-      <div>
-        <label for="user-birthdate">Gimino metai*</label>
-        <Datepicker
-          id="user-birthdate"
-          v-model="user.birthDate"
-          format="yyyy-MM-dd"
-          :monday-first="true"
-        />
-      </div>
+        <div class="flex align-baseline">
+          <label for="user-birthdate">Gimino metai*</label>
+          <Datepicker
+            id="user-birthdate"
+            v-model="user.birthDate"
+            format="yyyy-MM-dd"
+            :monday-first="true"
+          />
+        </div>
 
-      <div>
-        <label for="contacts-city">Miestas</label>
-        <input type="text" id="city" v-model.trim="user.contacts.city" />
-      </div>
+        <div class="flex align-baseline">
+          <label for="contacts-city">Miestas</label>
+          <input type="text" id="city" v-model.trim="user.contacts.city" />
+        </div>
 
-      <div>
-        <label for="contacts-phone">Telefonas</label>
-        <input
-          type="text"
-          id="contacts-phone"
-          v-model.trim="user.contacts.phone"
-        />
-      </div>
+        <div class="flex align-baseline">
+          <label for="contacts-phone">Telefonas</label>
+          <input
+            type="text"
+            id="contacts-phone"
+            v-model.trim="user.contacts.phone"
+          />
+        </div>
 
-      <div>
-        <label for="contacts-email">Elektroninis paštas</label>
-        <input
-          type="text"
-          id="contacts-email"
-          v-model.trim="user.contacts.email"
-        />
-      </div>
+        <div class="flex align-baseline">
+          <label for="contacts-email">Elektroninis paštas</label>
+          <input
+            type="text"
+            id="contacts-email"
+            v-model.trim="user.contacts.email"
+          />
+        </div>
 
-      <div>
-        <label for="contacts-facebook">Facebook paskyra</label>
-        <input
-          type="text"
-          id="contacts-facebook"
-          v-model.trim="user.contacts.facebook"
-        />
-      </div>
+        <div class="flex align-baseline">
+          <label for="contacts-facebook">Facebook paskyra</label>
+          <input
+            type="text"
+            id="contacts-facebook"
+            v-model.trim="user.contacts.facebook"
+          />
+        </div>
 
-      <div>
-        <label for="user-contact">Susisiekite su manimi*</label>
-        <select id="user-contact" v-model="user.contactMethod">
-          <option
-            v-for="(option, index) in contactOptions"
-            :key="index"
-            :value="option"
-          >
-            {{ option }}
-          </option>
-        </select>
-      </div>
+        <div class="flex align-baseline">
+          <label for="user-contact">Susisiekite su manimi*</label>
+          <select id="user-contact" v-model="user.contactMethod">
+            <option
+              v-for="(option, index) in contactOptions"
+              :key="index"
+              :value="option"
+            >
+              {{ option }}
+            </option>
+          </select>
+        </div>
 
-      <div>
-        <label for="user-about">Apie mane</label>
-        <textarea id="user-about" rows="3" v-model.trim="user.about" />
-      </div>
+        <div class="flex align-baseline">
+          <label for="user-about">Apie mane</label>
+          <textarea id="user-about" v-model.trim="user.about" />
+        </div>
 
-      <label
-        ><input
-          type="checkbox"
-          v-model="user.isDriver"
-          v-on:change="checkboxChanged"
-        />
-        Esu vairuotojas
-      </label>
+        <label>
+          <input
+            type="checkbox"
+            v-model="user.isDriver"
+            v-on:change="checkboxChanged"
+          />
+          Esu vairuotojas
+        </label>
 
-      <div v-if="!user.isDriver">
-        <Button
-          text="panaikinti profilį"
-          :isOutlined="true"
-          :isSecondary="true"
-          :click="deleteProfile"
-        />
-        <Button text="išsaugoti" :isOutlined="true" :click="saveProfile" />
+        <div
+          v-if="!user.isDriver"
+          class="flex align-baseline justify-end mt-50"
+        >
+          <Button
+            text="panaikinti profilį"
+            :isOutlined="true"
+            :isSecondary="true"
+            :click="deleteProfile"
+            class="mr-20"
+          />
+          <Button text="išsaugoti" :isOutlined="true" :click="saveProfile" />
+        </div>
       </div>
     </div>
 
     <div v-if="user.isDriver">
-      <h3>Vairuotojo profilis</h3>
+      <h3 class="section-title">Vairuotojo profilis</h3>
+      <div class="flex pb-50">
+        <div class="flex direction-column align-center">
+          <Avatar :path="user.driver.car.photo" size="big" />
+          <Button
+            :click="changeCarPhoto"
+            text="įkelti nuotrauką"
+            :isOutlined="true"
+            class="mt-20"
+          />
+        </div>
 
-      <div>
-        <Avatar :path="user.driver.car.photo" size="big" />
-        <Button
-          :click="changeCarPhoto"
-          text="įkelti nuotrauką"
-          :isOutlined="true"
-        />
-      </div>
+        <div class="flex direction-column grow">
+          <div class="flex align-baseline">
+            <label for="car-model">Transporto priemonė*</label>
+            <input
+              type="text"
+              id="car-model"
+              v-model.trim="user.driver.car.model"
+            />
+          </div>
 
-      <div>
-        <label for="car-model">Transporto priemonė*</label>
-        <input
-          type="text"
-          id="car-model"
-          v-model.trim="user.driver.car.model"
-        />
-      </div>
+          <div class="flex align-baseline">
+            <label for="car-date">Pagaminimo metai*</label>
+            <Datepicker
+              id="car-date"
+              v-model="user.driver.car.date"
+              format="yyyy-MM-dd"
+              :monday-first="true"
+            />
+          </div>
 
-      <div>
-        <label for="car-date">Pagaminimo metai*</label>
-        <Datepicker
-          id="car-date"
-          v-model="user.driver.car.date"
-          format="yyyy-MM-dd"
-          :monday-first="true"
-        />
-      </div>
+          <div class="flex align-baseline">
+            <label for="driver-contact">Susisiekite su manimi*</label>
+            <select id="driver-contact" v-model="user.driver.contactMethod">
+              <option
+                v-for="(option, index) in contactOptions"
+                :key="index"
+                :value="option"
+              >
+                {{ option }}
+              </option>
+            </select>
+          </div>
 
-      <div>
-        <label for="driver-contact">Susisiekite su manimi*</label>
-        <select id="driver-contact" v-model="user.driver.contactMethod">
-          <option
-            v-for="(option, index) in contactOptions"
-            :key="index"
-            :value="option"
-          >
-            {{ option }}
-          </option>
-        </select>
-      </div>
+          <div class="flex align-baseline">
+            <label for="driver-about">Vairavimo įgūdžiai</label>
+            <textarea
+              id="driver-about"
+              rows="3"
+              v-model.trim="user.driver.about"
+            />
+          </div>
 
-      <div>
-        <label for="driver-about">Vairavimo įgūdžiai</label>
-        <textarea id="driver-about" rows="3" v-model.trim="user.driver.about" />
-      </div>
-
-      <div>
-        <Button
-          text="panaikinti profilį"
-          :isOutlined="true"
-          :isSecondary="true"
-          :click="deleteProfile"
-        />
-        <Button text="išsaugoti" :isOutlined="true" :click="saveProfile" />
+          <div class="flex align-baseline justify-end mt-50">
+            <Button
+              text="panaikinti profilį"
+              :isOutlined="true"
+              :isSecondary="true"
+              :click="deleteProfile"
+              class="mr-20"
+            />
+            <Button text="išsaugoti" :isOutlined="true" :click="saveProfile" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -220,3 +235,7 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+@import '../assets/styles/user-edit.scss';
+</style>
