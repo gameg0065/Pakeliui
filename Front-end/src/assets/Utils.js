940c8dbad6561200e2307e92f584425ce118712d
Tomas Sinkunas <Tomas@rendertom.com>
2020-11-27 18:53:07 +0200

    adds new method

--- Front-end/src/assets/Utils.js
+++ Front-end/src/assets/Utils.js
@@ -1,4 +1,11 @@
 export default {
+	calculateAge(birthDate) {
+		birthDate = birthDate instanceof Date ? birthDate : new Date(birthDate);
+
+		const ageDifMs = Date.now() - birthDate.getTime();
+		const ageDate = new Date(ageDifMs);
+		return Math.abs(ageDate.getUTCFullYear() - 1970);
+	},
 	validateEmail(email) {
 		//eslint-disable-next-line
 		const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
