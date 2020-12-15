e31189349a73faa8f03ffac397e0a184444cecac
Tomas Sinkunas <Tomas@rendertom.com>
2020-12-15 22:01:59 +0200

    add date converter

--- Front-end/src/assets/Utils.js
+++ Front-end/src/assets/Utils.js
@@ -19,6 +19,14 @@ export default {
 
 		return this.mergeDeep(target, ...sources);
 	},
+	toDateString(date) {
+		if (!date) {
+			return '';
+		}
+		
+		date = date instanceof Date ? date : new Date(date);
+		return date.toISOString();
+	},
 	validateEmail(email) {
 		//eslint-disable-next-line
 		const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
