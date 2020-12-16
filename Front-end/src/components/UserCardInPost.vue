632ddcec38977975373fb3b30b5baa9803dd4c51
Tomas Sinkunas <Tomas@rendertom.com>
2020-12-16 17:49:20 +0200

    update UserCardInPost.vue component

--- Front-end/src/components/UserCardInPost.vue
+++ Front-end/src/components/UserCardInPost.vue
@@ -1,46 +1,41 @@
 <template>
   <div class="card shadow flex">
-    <Avatar :path="user.photo" class="mr-20"/>
+    <Avatar :path="user.picturePath" class="mr-20" />
 
     <div class="flex direction-column grow">
-      <router-link :to="{ name: 'user', params: { id: user.id } }">
+      <router-link :to="{ name: 'user', params: { id: user.userId } }">
         <h4 class="text-color-primary mb-10">{{ user.name }}</h4>
       </router-link>
 
-      <small> Pavėžėjimų skaičius: {{ this.user.driver.posts.length }}</small>
+      <small> Pavėžėjimų skaičius: {{ this.user.posts.length }}</small>
 
       <p>Susisiekite su manimi: {{ getContactInfo() }}</p>
     </div>
 
-    <Rating :rating="user.driver.rating" />
+    <!-- <Rating :rating="user.driver.rating" /> -->
   </div>
 </template>
 
 <script>
 import Avatar from '@/components/Avatar.vue';
-import Rating from '@/components/Rating.vue';
-
-import UserService from '@/services/UserService.js';
+// import Rating from '@/components/Rating.vue';
 
 export default {
   name: 'UserCardInPost',
   props: ['post'],
   components: {
     Avatar,
-    Rating,
+    // Rating,
   },
   data() {
     return {
-      user: Object,
+      user: this.post.user,
     };
-  },
-  created() {
-    this.user = UserService.getUser(this.post.driver.id);
   },
   methods: {
     getContactInfo() {
-      const contactMethod = this.user.driver.contactMethod;
-      const contactInfo = this.user.contacts[contactMethod];
+      const contactMethod = this.user.driverContactMethod;
+      const contactInfo = this.user[contactMethod];
       return contactInfo;
     },
   },
