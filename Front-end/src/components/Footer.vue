a4a91321cf46bf9dc3a5de02024f6203c6f9cef6
Tomas Sinkunas <Tomas@rendertom.com>
2020-12-20 18:54:12 +0200

    cleanup Footer.vue component

--- Front-end/src/components/Footer.vue
+++ Front-end/src/components/Footer.vue
@@ -3,7 +3,6 @@
     <p>©2020 KA Dream Team</p>
     <ul>
       <li><router-link :to="{ name: 'about' }">apie mus</router-link></li>
-      <!-- <li><router-link :to="{ name: 'contacts' }">kontaktai</router-link></li> -->
       <li><router-link :to="{ name: 'terms' }">sąlygos</router-link></li>
     </ul>
   </div>
