1a1774708df6a58a8776893ca6cc149ba10759b7
Tomas Sinkunas <Tomas@rendertom.com>
2020-12-20 18:53:33 +0200

    cleanup DriverFeedbackCard.vue

--- Front-end/src/components/DriverFeedbackCard.vue
+++ Front-end/src/components/DriverFeedbackCard.vue
@@ -6,29 +6,13 @@
         <router-link :to="{ name: 'user', params: { id: passengerId } }">
           <p class="text-color-primary">{{ user.name }}</p>
         </router-link>
-        <!-- <small v-if="feedback"> Įvertinimo data: {{ feedback.date }} </small> -->
       </div>
-
-      <!-- <Rating v-if="feedback" :rating="feedback.rating" />
-      <Button
-        v-else
-        text="įvertink keleivį"
-        :click="rateUser"
-        :isOutlined="true"
-        :isLarge="false"
-      /> -->
     </div>
-
-    <!-- <p v-if="feedback">{{ feedback.text }}</p> -->
   </div>
 </template>
 
 <script>
 import Avatar from '@/components/Avatar.vue';
-// import Button from '@/components/Button.vue';
-// import Rating from '@/components/Rating.vue';
-
-// import FeedbackService from '@/services/FeedbackService.js';
 
 import Service from '@/services/Service';
 
@@ -37,13 +21,10 @@ export default {
   props: ['passengerId'],
   components: {
     Avatar,
-    // Button,
-    // Rating,
   },
   data() {
     return {
       user: {},
-      feedback: null,
     };
   },
   async created() {
@@ -56,28 +37,6 @@ export default {
       .catch((error) => {
         console.log('Could not get user by ID', error);
       });
-
-    // if (this.passenger.feedback) {
-    //   this.feedback = FeedbackService.getFeedback(this.passenger.feedback.id);
-    // }
-  },
-  methods: {
-    // rateUser() {
-    //   const modal = this.$modal;
-    //   modal.show('modal-feedback', {
-    //     title: 'Atsiliepimas',
-    //     text: 'Atsiliepimo pakeisti nebus galima.',
-    //     ratingTitle: 'Įvertinkite keleivį',
-    //     user: this.user,
-    //     button: {
-    //       title: 'skelbti atsiliepimą',
-    //       action(data) {
-    //         alert('TODO');
-    //         modal.hide('modal-feedback');
-    //       },
-    //     },
-    //   });
-    // },
   },
 };
 </script>
